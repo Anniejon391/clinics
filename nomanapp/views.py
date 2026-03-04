f41194fcd1cbc2043bf843d518b7b19e92f26bfa
Anniejon391 <anniejon391@gmail.com>
2026-03-04 15:21:03 +0530

    first commit

---
+++ nomanapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
+def contact(request):
+    if request.method == "POST":
+       message_name = request.POST['message-name']
+       message_email = request.POST['message-name']
+       message_subject = request.POST['message-name']
+       umessage = request.POST['umessage']
+
+       send_mail(
+           message_name, #user name
+           message_subject, # message subject
+           umessage, # messages
+           message_email, # from email
+           ['villanbilly70@gmail.com'], # To email
+       )
+
+       return render(request, 'contact.html', {'message_name': message_name})
+       
+    else:
+        return render(request, 'contact.html', {})
